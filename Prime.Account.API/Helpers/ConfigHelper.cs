3d33135cd49d40fb43d6ea32eebe59cd35f9a828
Ralf Von R. Luzuriaga <luzuriagarv@outlook.com>
2020-04-21 21:12:47 +0800

    Add Configuration Helper class to handle Web.config values

---
+++ Prime.Account.API/Helpers/ConfigHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+namespace Prime.Account.API.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static string GetIssuer => ConfigurationManager.AppSettings["Issuer"];
+        public static string GetSecretKey => ConfigurationManager.AppSettings["Secret"];
+        public static string GetAudience => ConfigurationManager.AppSettings["Audience"];
+        public static string GetEndpointPath => ConfigurationManager.AppSettings["EndpointPath"];
+        public static string GetTokenExpireTimeSpan => ConfigurationManager.AppSettings["TokenExpireTimeSpan"];
+        public static string GetAllowedOrigin => ConfigurationManager.AppSettings["AllowedOrigin"];
+        public static string GetAuthType => ConfigurationManager.AppSettings["AuthenticationType"];
+        public static string GetOAuthError => ConfigurationManager.AppSettings["OAuthError"];
+        public static string GetOAuthErrorDesc => ConfigurationManager.AppSettings["OAuthErrorDescription"];
+        public static string GetOAuthErrorStatCode => ConfigurationManager.AppSettings["OAuthResponseStatusCode"];
+    }
+}
